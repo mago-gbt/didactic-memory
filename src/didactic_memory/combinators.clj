228992beabd1dd2ed34a6624eaeb13c94e663d06
Marian Gönninger <marian.goenninger@goenninger.net>
2021-11-03 11:21:56 +0100

    intial, primitives

---
+++ src/didactic_memory/combinators.clj
@@ -0,0 +1,1 @@
+(ns didactic-memory.combinators)
---
+++ src/didactic_memory/primitives.clj
@@ -0,0 +1,26 @@
+(ns didactic-memory.primitives)
+
+(defn succeed [v]
+  (fn [inp]
+    [[v inp]]))
+
+(def fail
+  (fn [inp]
+    []))
+
+(defn satisfy [pred]
+  (fn [inp]
+    (let [[x & xs] inp]
+      (cond
+        (empty? inp) (fail inp)
+        :else (cond
+                (pred x) ((succeed x) xs)
+                :else (fail xs))))))
+
+(defn literal [x]
+  (satisfy #(= x %)))
+
+((literal \3) "345")
+
+(comment
+  (->> (succeed) \3 [45]))
